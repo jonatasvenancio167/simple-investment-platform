fa6a071c18af7df7fac30460bbb1a35670e1a708
Jonatas Venancio <jvenancio167@hotmail.com>
2025-10-29 17:48:36 -0300

    chore(config): remove popperjs core pin as it's no longer needed

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+ # JS do Bootstrap não é necessário para alerts dismissíveis; removido para evitar 404 de dependências.
