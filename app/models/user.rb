b5cec928440d71d37f9db8e0becbe0961f01d478
Jonatas Venancio <jvenancio167@hotmail.com>
2025-10-30 11:16:10 -0300

    fix(user): change investments association from restrict_with_error to destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  has_many :investments, dependent: :restrict_with_error
+  has_many :investments, dependent: :destroy
   
   validates :name, presence: true, length: { maximum: 255 }
   validates :email,
